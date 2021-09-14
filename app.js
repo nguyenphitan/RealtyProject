5337bd030af0a547a0db36fe84cbd54e5457b663
nguyenphitan <62367845+nguyenphitan@users.noreply.github.com>
2021-09-14 19:03:45 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+// Mobile
+// Header
+let navbar = document.querySelector('.navbar');
+let navList = document.querySelector('.nav-list');
+let exit = document.querySelector('.exit');
+let overlay = document.querySelector('.overlay');
+
+navbar.onclick = function() {
+    navbar.style.display = 'none';
+    navList.style.transform = 'translateX(0)';
+    navList.style.opacity = '1';
+    exit.style.display = 'block';
+    overlay.style.display = 'block';
+}
+
+overlay.onclick = () => {
+    whileOnClick();
+};
+
+exit.onclick = () => {
+    whileOnClick();
+}
+
+function whileOnClick() {
+    navbar.style.display = 'block';
+    navList.style.transform = 'translateX(100%)';
+    navList.style.opacity = '0';
+    exit.style.display = 'none';
+    overlay.style.display = 'none';
+}
+
+// 
